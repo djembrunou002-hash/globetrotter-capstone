0a0e9e7d61334e0df12b0570363da8df0d0ddb48
djembrunou002-hash <djembrunou002@gmail.com>
2026-07-24 21:13:41 +0100

    User token storage and attachment to forward requests

--- global_trotter_web/src/services/api.js
+++ global_trotter_web/src/services/api.js
@@ -1,9 +1,14 @@
+import { getToken } from './tokenStorage.js'
+
 export const API_BASE_URL = 'http://localhost:5000'
 
 export async function apiRequest(endpoint, options = {}) {
+  const token = getToken()
+
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
     headers: {
       'Content-Type': 'application/json',
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
       ...(options.headers || {})
     },
     ...options
