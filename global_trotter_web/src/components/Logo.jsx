78736a4876c42cfa4469b595b12836820c573ced
djembrunou002-hash <djembrunou002@gmail.com>
2026-07-25 22:45:53 +0100

    Updated Logo

--- global_trotter_web/src/components/Logo.jsx
+++ global_trotter_web/src/components/Logo.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router-dom'
+import { getToken } from '../services/tokenStorage.js'
 import '../styles/Logo.css'
 
 function Logo({ theme = 'light' }) {
+  const isAuthenticated = Boolean(getToken())
+
+  if (isAuthenticated) {
+    return <span className={`logo logo--${theme}`}>GlobalTrotter</span>
+  }
+
   return (
     <Link to="/" className={`logo logo--${theme}`}>
       GlobalTrotter
