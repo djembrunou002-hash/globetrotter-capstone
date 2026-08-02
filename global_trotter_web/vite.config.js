7df6b9b1f5d3678bd3f8cd83649a2369fb7bbb08
djembrunou002-hash <djembrunou002@gmail.com>
2026-08-02 13:26:23 +0100

    Scope maplibre optimization

--- global_trotter_web/vite.config.js
+++ global_trotter_web/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['maplibre-gl']
-  },
+  optimizeDeps: command === 'serve' ? { exclude: ['maplibre-gl'] } : {},
   server: {
     host: true
   },
-})
+}))
